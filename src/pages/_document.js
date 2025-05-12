aaadcc2e48772df776d3c226e8c97a04dde92a10
Stefan <stefan@innofaith.io>
2025-05-12 21:17:34 +0200

    Add Plausible Analytics script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         />
+        <script
+          defer
+          data-domain="innofaithrwa.com"
+          src="https://plausible.io/js/script.js"
+        ></script>
       </Head>
 
       <body>
